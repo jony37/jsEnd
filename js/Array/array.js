566667de64bf1abd4349b38f4125daf4f194634e
jony <ravshanovjony7@gmail.com>
2025-04-03 21:55:30 +0500

    Array

---
+++ js/Array/array.js
@@ -0,0 +1,65 @@
+
+// 13 DARS ARRAY
+
+// const colors = ["red", "blue", "black", "yellow", "green"];
+
+// console.log("Arraydi uzinligi:", colors.length);
+// console.log("Index yordamida elementni tanlab olad:", colors[1]);
+// console.log("Arraydi oxiriga yangi element qoshadi:", colors.push("brown"));
+// console.log("Arraydi oxirigi elementini ochiradi", colors.pop());
+// console.log("Arraydi boshiga yangi element qoshadi:", colors.unshift("gray"));
+// console.log("Arraydi boshidagi elementni ochiradi:", colors.shift());
+
+// const cars = ["Porshe", "Merc", "BMW", "Audi", "Suzuki"];
+
+// ИТЕРИЦИЯ
+
+// 1
+// for (let i = 0; i < cars.length; i++) {
+//     console.log(cars[i]);
+// }
+
+// // 2
+// for (let car of cars) {
+//     console.log(car);
+// }
+
+// // 3 method
+// colors.forEach((item, index, arr) => {
+//     console.log(`${index}: ${item} in array ${arr}`);
+
+// })
+
+// const numebers = [1,2,3,4,5]
+// numebers[99] = 100; // BU USULDAN MASIVGA ELEMENT QUSHISH UMUMAN TAVSIYA ETILMEDI
+// console.log(numebers.length);
+// console.log(numebers);
+
+// // Split
+
+// const socialMedia = "Youtibe, Telegram, Instagram"
+// const arr = socialMedia.split(', ')
+// const join = arr.join(" ")
+// console.log(arr);
+// console.log(join);
+
+// SORT()
+
+// const cars = ["Porshe", "Merc", "BMW", "Audi", "Suzuki"];
+// cars.sort();
+// console.log(cars);
+
+// // sort methodi number korinishidagi malumot turuda xatolik bilan ishlaydi
+// // chunki u numberimizdi faqatgini 1 chi elementini tanlab oladi masalan 12 bulsa u faqatgina 1 di tanlab oladi
+
+// const n = [23, 34, 7, 12, 74, 5];
+
+// n.sort(compareNum)
+// console.log(n);
+
+
+// // Bu funksiya sortni xatoligini yoqotib beradi
+// function compareNum (a, b) {
+//     return a - b;
+// }
+
